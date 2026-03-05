4d099e6d62433b7d63042c5971bd80353c88b3bd
Andy <agcayabyab@carc.pshs.edu.ph>
2026-03-05 19:54:45 +0800

    Update: original code had 24 fatal errors, new code skeleton with 14 segmented functions added. Weapons and Bosses are updated, the player can now create new profiles at the cost of their current progress. Still fixing json functions though.. DO NOT CHANGE THE FINAL JSON FILE UNLESS FLAWS ARE PRESENT. Still working on several new functions, but will update in maybe a few hours again. Committed by Augusto Cayabyab

--- list experiments.py
+++ list experiments.py
@@ -1,95 +1,113 @@
+import json
 import random
-tasks = []
-php = 100
-chp = 120
-print("1. Add Task\n"
-      "2. Delete Task\n"
-      "3. Edit Task name\n"
-      "4. Complete Task\n"
-      "5. Fight\n"
-      "6. Exit Taskmaster")
-choice = int(input("Enter a choice from 1 - 6: "))
-while choice != 7 or (choice < 1 or choice >6):
-    if choice == 1:
-        task = input('Enter a task to add to to-do list and "0" to stop: ')
-        while task != "0" or len(tasks) < 12:
-            if task == "0":
-                break
-            else:
-                print(f"Task '{task}' added to to-do list.")
-                print("Task list: ")
-                tasks.append(task)
-                for i in range(0,len(tasks)):
-                    print(f"{i + 1}. {tasks[i]}")
-                task = input('Enter a task to add to to-do list and "0" to stop: ')
-                if task == "0":
-                    break
-    elif choice == 2:
-        for i in range(0,len(tasks)):
-            print(f"{i + 1}. {tasks[i - 1]}")
-        remove = int(input("Enter task number to remove or '0' to stop: "))
-        while remove <= len(tasks) or len(tasks) != 0 or remove < 1 or remove != 0:
-            if remove == 0:
-                break
-            print(f'Task "{tasks[remove - 1]}" removed.')
-            del tasks[remove - 1]
-            for i in range(0,len(tasks)):
-                    print(f"{i + 1}. {tasks[i]}")
-            remove = int(input("Enter task number to remove or '0' to stop: "))
-    elif choice == 3:
-        for i in range(0,len(tasks)):
-            print(f"{i + 1}. {tasks[i - 1]}")
-        edit = int(input("Enter task number to edit or '0' to stop: "))
-        while edit <= len(tasks) or len(tasks) != 0 or edit< 1:
-            tasks[edit - 1] = input("Enter new task name: ")
-            print(f'Task "{tasks[edit - 1]}" edited is the new name of task no. {edit}.')
-            for i in range(0,len(tasks)):
-                print(f"{i + 1}. {tasks[i ]}")
-            edit = int(input("Enter task number to edit or '0' to stop: "))
-    elif choice == 4:
-        for i in range(0,len(tasks)):
-            print(f"{i + 1}. {tasks[i ]}")
-        complete = int(input("Enter task number accomplished or '0' to stop: "))
-        while edit <= len(tasks) or len(tasks) != 0 or edit< 1:
-            print(f'Task "{tasks[edit - 1]}" accomplished.')
-            del tasks[edit - 1]
-            for i in range(0,len(tasks)):
-                print(f"{i + 1}. {tasks[i ]}")
-            complete = int(input("Enter task number accomplished or '0' to stop: "))
-    elif choice == 5:
-        pattack = int(input("Enter 1 for Blade, 2 for Bow, or 3 for Shield."))
-        while chp > 0 or php > 0:
-            attacks = ["Blade", "Bow", "Shield"]
-            cattack = random.randint(0,2)
-            if pattack == cattack:
-                print(f" Player chose: {attacks[pattack - 1]}")
-                print(f" Computer chose: {attacks[cattack]}")
-                chp -= 5
-                php -= 5
-                print(f"Computer: {chp}")
-                print(f"Player: {php}")
-            if attacks[pattack - 1] == "Blade" and attacks[cattack] == "Bow" or attacks[pattack - 1] == "Shield" and attacks[cattack] == "Bow" or attacks[cattack - 1] == "Blade" and attacks[pattack] == "Shield":
-                print(f" Player chose: {attacks[pattack - 1]}")
-                print(f" Computer chose: {attacks[cattack]}")
-                php -= 10
-                print(f"Computer: {chp}")
-                print(f"Player: {php}")
-            if attacks[cattack ] == "Blade" and attacks[pattack-1] == "Bow" or attacks[cattack ] == "Shield" and attacks[pattack - 1] == "Bow" or attacks[cattack ] == "Blade" and attacks[cattack] == "Shield":
-                print(f" Player chose: {attacks[pattack - 1]}")
-                print(f" Computer chose: {attacks[cattack]}")
-                chp -= 10
-                print(f"Computer: {chp}")
-                print(f"Player: {php}")
-            pattack = int(input("Enter 1 for Blade, 2 for Bow, or 3 for Shield."))
-            if chp == 0:
-                print ('Player wins!')
-                break
-            elif php == 0:
-                print('Computer Wins!')
-                break
-            pattack = int(input("Enter 1 for Blade, 2 for Bow, or 3 for Shield."))
-    choice = int(input("Enter a choice from 1 - 6: "))
-print('Goodbye, user!')
+import time
+print("WELCOME TO TASMKMASTER!")
+print("A project by: Augusto Alfonso Cayabyab, Juan Miguel Rivera, and Gilian Uoiea Janiola!")
+print("For starters, this is the whole point of taskmaster: ELIMINATE PROCRASTINATION!")
+def load_game():
+    global data
+    with open("player.json", "r") as f:
+        data = json.load(f)
+
+def save_game(data):
+    with open("players.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+def reset_game():
+    with open("players.json", "r") as f:
+        data = json.load(f)
+
+
+def create_player(username, password):
+
+    done = False
+    while not done:
+        data[0]["player"]["username"] = input("Enter your preferred Hero Title: ")
+        passlogin = input("Enter your Hero's Code (password): ")
+
+def sign_in():
+    done = False
+    while not done:
+        userlogin = input("Enter your Hero Title: ")
+        passlogin = input("Enter your Hero's Code (password): ")
+        if userlogin == data[0]["player"] and passlogin
+
+def show_menu():
+    print("1. New Password/ New Username\n"
+          "2. Save/Exit Game\n"
+          "3. View stats\n"
+          "4. Add a new task to task list\n"
+          "5. Mark task as completed\n"
+          "6. Fight Boss\n"
+          "7. New Profile(reset stats)\n")
+    choice = int(input("Enter your choice: "))
+    return choice
+
+
+def view_stats(data):
+    for player in data["player"]:
+        print(player["username"])
+        for i in player["tasks_to_do"]:
+            print(f"{i+1}: {player["tasks_to_do"][i]}")
+        print(player["tasks_done"])
+
+
+def add_task(data):
+    pass
+
+
+def complete_task(data):
+    pass
+
+
+def choose_weapon():
+    pass
+
+
+def enemy_choose_weapon():
+    pass
+
+
+def resolve_round(player_weapon, enemy_weapon):
+    pass
+
+
+def boss_round():
+    pass
+
+
+def boss_battle(data, level_number):
+    pass
+
+
+def can_fight_boss(data, level_number):
+    pass
+
+
+def pay_boss_cost(data, level_number):
+    pass
+
+
+def give_boss_reward(data, level_number):
+    pass
+
+
+def rebirth_player(data):
+    pass
+
+
+def level_up(data):
+    pass
+
+
+def check_true_ending(data):
+    pass
+
+
+def game_loop():
+    pass
+
+
 
 
 
